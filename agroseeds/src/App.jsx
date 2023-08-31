6fc4effb137df5d8b7940e012865470ddb699af9
moichubr <brunmoira@gmail.com>
2023-08-31 15:02:00 -0300

    components

--- agroseeds/src/App.jsx
+++ agroseeds/src/App.jsx
@@ -1,4 +1,11 @@
 import { useState } from 'react'
+import Landing from './Components/Landing'
+import AboutUs from './Components/AboutUs'
+import Services from './Components/Services'
+import Gallery from './Components/Gallery'
+import Contact from './Components/Contact'
+import Navbar from './Components/Navbar'
+
 
 import './App.css'
 
@@ -6,9 +13,13 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Esto es Agroseed</h1>
-    </>
+    <main>
+      <Navbar />
+      <Landing />
+      <Services />
+      <Gallery />
+      <Contact />
+    </main>
   )
 }
 
