c81dbc43723a9ac7cd81f14e8f5743a7b28b1ba2
moichubr <brunmoira@gmail.com>
2023-09-21 17:46:44 -0300

    viteconfig modified

--- agroseeds/vite.config.js
+++ agroseeds/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+//le agregue build y server para produccion
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Carpeta de salida de construcción
+  },
+  server: {
+    port: 4173, // Puerto de desarrollo local
+  },
+  
 })
