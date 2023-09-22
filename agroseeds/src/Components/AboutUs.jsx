964a93b843273dc906d2ce9a72cf5ff76f9b7d2b
moichubr <brunmoira@gmail.com>
2023-09-22 16:06:48 -0300

    retoques

--- agroseeds/src/Components/AboutUs.jsx
+++ agroseeds/src/Components/AboutUs.jsx
@@ -5,13 +5,13 @@ const AboutUs = () => {
 //actualmente
     return(
         <section id="about" style={{ backgroundImage: `url(${bg})` }} className="bg-no-repeat  object-contains ">
-    <div className=" text-center text-green-900 pt-8">
+    <div className=" text-center text-green-900 pt-24">
     <span className="lg:text-4xl text-3xl px-4 rounded-md font-extralight">LA <span className="font-semibold">EMPRESA</span></span>
     </div>
           <div className="lg:grid lg:grid-cols-2 lg:w-6/8">
 
-  <div className="lg:col-span-1 text-white lg:h-auto text-center font-thin lg:py-10 pt-16 pb-4">
-    <div className="flex flex-col items-center lg:float-right lg:mr-6 lg:w-2/3 mx-auto px-2 w-full text-md lg:mt-10">
+  <div className="lg:col-span-1 text-white lg:h-auto text-center font-thin lg:py-10 pt-6 pb-4">
+    <div className="flex flex-col items-center lg:float-right lg:mr-6 lg:w-2/3 mx-auto px-2 py-auto w-full text-md lg:mt-10">
       <p className="pt-4">
         Nuestro origen se remonta a mas de 60 años atrás, cuando las máquinas eran a vapor y se cosechaba a mano, bajo la firma Orlando Bouvet e Hijos S.R.L. Durante muchos años nos hemos destacado en la labor agrícola en Entre Ríos y Corrientes.
       </p>
--- agroseeds/src/Components/WhatsappBtn.jsx
+++ agroseeds/src/Components/WhatsappBtn.jsx
@@ -3,7 +3,7 @@ import {BsWhatsapp} from "react-icons/bs"
 const WhatsappBtn = () => {
 
     return(
-        <div className="fixed w-[60px] h-[60px] lg:bottom-[40px] bottom-[100px] lg:right-[40px] right-[50px] bg-green-600 text-white rounded-full shadow-lg z-100 flex items-center justify-center hover:bg-white hover:text-green-600">
+        <div className="fixed w-[60px] h-[60px] lg:bottom-[40px] bottom-[80px] lg:right-[40px] right-[30px] bg-green-600 text-white rounded-full shadow-lg z-100 flex items-center justify-center hover:bg-white hover:text-green-600">
         <a href="https://api.whatsapp.com/send?phone=3447640311" target="_blank" rel="noreferrer">
           <i><BsWhatsapp size={40} /></i>
         </a>
